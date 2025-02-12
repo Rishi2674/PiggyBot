c14c39b46c7842255169295e9868bc48cf3a7489
Rishi Upadhyay <rupadhyay2674@gmail.com>
2025-02-12 11:45:30 +0530

    added config apis

--- config/config.py
+++ config/config.py
@@ -7,7 +7,9 @@ load_dotenv()
 # Configuration Variables
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "default_token")
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-GEMINI_API_KEY = os.getenv("GEMINI_API", "gemini_api_key")
+GEMINI_API_KEY_1 = os.getenv("GEMINI_API_1", "gemini_api_key_1")
+GEMINI_API_KEY_2 = os.getenv("GEMINI_API_2", "gemini_api_key_2")
+GEMINI_API_KEY_3 = os.getenv("GEMINI_API_3", "gemini_api_key_3")
 WHATSAPP_API_KEY = os.getenv("WHATSAPP_API_KEY", "whatsapp_api_key")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID", "phone number id")
 
